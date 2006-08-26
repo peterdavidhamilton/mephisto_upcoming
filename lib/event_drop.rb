721480417fa51ee85265c12931ce7b5e6c8df367
danwebb <danwebb@a686c101-5f17-0410-80e1-b267258107f6>
2006-08-26 00:31:52 +0000

    more stuff

---
+++ lib/event_drop.rb
@@ -0,0 +1,56 @@
+module MephistoUpcoming
+  class EventDrop > Liquid::Drop
+    include Mephisto::Liquid::DropMethods
+  
+    def event
+      @source
+    end
+  
+    def initialize(source)
+      @source = source
+      @event_liquid = {
+        'name' => @source.name,
+        'tags' => @source.tags.split(' '),
+        'description' => @source.description,
+        'start' => @source.start,
+        'end' => @source.end, 
+        'status' => @source.status
+      }
+    end
+    
+    def before_method(meth)
+      @event_liquid[meth.to_s]
+    end
+  
+    def venue
+      @venue ||= @source.venue.to_liquid
+    end
+  
+  end
+end
+
+class Upcoming::Event
+  
+  def to_liquid
+    MephistoUpcoming::EventDrop.new(self)
+  end
+  
+end
+
+class Upcoming::Venue
+  
+  def to_liquid
+    {
+      'name' => name, 
+      'address' => address, 
+      'city' => city, 
+      'zip' => zip, 
+      'phone' => phone, 
+      'url' => url, 
+      'description' => description, 
+      'longitude' => longitude, 
+      'latitude' => latitude
+    }
+  end
+  
+end
---
+++ lib/upcoming_watchlist.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require_gem 'upcoming'
+require 'liquid_tag_base/liquid_collection_tag_base'
+require 'liquidize_events'
+
+module MephistoUpcoming
+  
+  class UpcomingWatchlistError < StandardError; end
+  
+  def self.authenticate(options={})
+    UpcomingWatchlist.token = if options[:frob]
+      Upcoming::authenticate_with_frob(options[:api_key], options[:frob])
+    elsif options[:token]
+      Upcoming::authenticate(options[:api_key], options[:token])
+    else
+      raise UpcomingWatchlistError, "Incorrect upcoming authentication details given.  You must supply an api_key and either a frob or token.  Man, don't ask me. It's well over complicated."
+    end
+  end
+  
+  class UpcomingWatchlist < Liquid::CollectionTagBase
+    
+    @@token = nil
+    cattr_accessor :token
+    
+    def render(context)
+      raise UpcomingWatchlistError, "Please supply Upcoming authentication details." unless token
+      result, options = [], evaluate(@options, context)
+      watchlist = Upcoming::User.get_watchlist(token.user_id)
+      watchlist = watchlist.select { |event| event.status == option[:status] } if status
+      watchlist = watchlist[0..options[:count] - 1] if options[:count]
+      
+      watchlist.collect(&:to_liquid).each_with_index do |item, index|
+        context.stack do
+          context[tagname] = { 'index' => index + 1 }
+          context[@as] = item
+          result << render_all(@nodelist, context)
+        end
+      end
+      
+      result
+    end
+  
+end
